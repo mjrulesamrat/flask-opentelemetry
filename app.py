1ed2eb22da7ad5c9cff329b764512e139aaca7e1
Jay Modi <mjrulesamrat@gmail.com>
2021-04-03 12:54:19 +0530

    Integrate Exporter: Jaeger
    - Add jaeger docker container running guideline
    - Add one more span to understand request-response trace

--- app.py
+++ app.py
@@ -4,24 +4,39 @@ import os
 import requests
 
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource as REST_Resource, Api
 from dotenv import load_dotenv
 
 from opentelemetry import trace
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import (
     ConsoleSpanExporter,
     SimpleSpanProcessor,
+    BatchSpanProcessor
 )
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
 
 load_dotenv()  # take environment variables from .env
 
 # set trace provider as default one
-trace.set_tracer_provider(TracerProvider())
+trace.set_tracer_provider(
+    TracerProvider(
+        resource=Resource.create({SERVICE_NAME: "my-helloworld-service"})
+    )
+)
+
+# jaeger exporter
+jaeger_exporter = JaegerExporter(
+    agent_host_name="localhost",
+    agent_port=6831,
+)
+
+# add exporter to trace
 trace.get_tracer_provider().add_span_processor(
-    SimpleSpanProcessor(ConsoleSpanExporter())
+    BatchSpanProcessor(jaeger_exporter)
 )
 
 app = Flask(__name__)
@@ -31,12 +46,14 @@ FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 
 
-class HelloWorld(Resource):
+class HelloWorld(REST_Resource):
     def get(self):
         tracer = trace.get_tracer(__name__)
         with tracer.start_as_current_span("example-request"):
             requests.get("http://www.example.com")
-            print("Hello World")
+        with tracer.start_as_current_span("get-oranges"):
+            data = 100*100
+            print("Hello World", data)
         return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/')
